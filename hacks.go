837b0f49195dd9c793a7a74db88ee9d5c4acf27c
Maxim Lebedev <git@toby3d.me>
2024-02-09 11:37:08 +0600

    :memo: Added package docs

---
+++ hacks.go
@@ -0,0 +1,9 @@
+// The hacks package contains various utilities for common and typical cases.
+//
+// This package and its sub-packages are "perpetually unstable": the composition
+// of packages and their API's change without warning or tag versioning,
+// depending on personal needs, requirements and preferences, as well as when
+// official packages emerge that duplicate the behavior of current ones. Use
+// vendoring to fix the specific state of the desired package, or simply copy
+// the required utilities manually.
+package hacks
